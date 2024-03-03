df450f227dba9157c0e20fff363a06da9baebb3f
vethilm <burnsc376@gmail.com>
2024-03-03 16:19:00 -0500

    site setup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mobileNav() {
+    let x = document.getElementById("menus");
+
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
